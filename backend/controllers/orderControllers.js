14785452532ec605b458184468b9a041fcbd32bf
Usman3432 <u.ghani3132@gmail.com>
2025-09-27 20:13:42 +0500

    added user Order routes

--- backend/controllers/orderControllers.js
+++ backend/controllers/orderControllers.js
@@ -3,35 +3,55 @@ import Order from "../models/orderModel.js";
 import ErrorHandler from "../utils/error_handler.js";
 import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
 
-
-
 //Create new Order  => /api/v1/order/new
-export const newOrder = catchAsyncErrors(async(req, res, next)=>{
-    const {
-        orderItems,
-        shippingInfo,
-        itemPrice,
-        taxAmount,
-        shippingAmount,
-        totalAmount,
-        paymentMethod,
-        paymentInfo
-    } = req.body
+export const newOrder = catchAsyncErrors(async (req, res, next) => {
+  const {
+    orderItems,
+    shippingInfo,
+    itemPrice,
+    taxAmount,
+    shippingAmount,
+    totalAmount,
+    paymentMethod,
+    paymentInfo,
+  } = req.body;
 
-    const order = await Order.create({
-        orderItems,
-        shippingInfo,
-        itemPrice,
-        taxAmount,
-        shippingAmount,
-        totalAmount,
-        paymentMethod,
-        paymentInfo,
-        user: req.user._id
-    });
+  const order = await Order.create({
+    orderItems,
+    shippingInfo,
+    itemPrice,
+    taxAmount,
+    shippingAmount,
+    totalAmount,
+    paymentMethod,
+    paymentInfo,
+    user: req.user._id,
+  });
 
+  res.status(200).json({
+    order,
+  });
+});
 
-    res.status(200).json({
-        order
-    })
+// Get order Details        => /api/v1/orders/:id
+export const getOrderDetails = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+  if (!order) {
+    return next(new ErrorHandler("Order did not found with this id", 404));
+  }
+
+  res.status(200).json({
+    order,
+  });
+});
+
+// Get current user orders        => /api/v1/me/orders
+export const userOrderDetails = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.find({ user: req.user._id });
+  res.status(200).json({
+    order,
+  });
 });
--- backend/rotues/orderRoutes.js
+++ backend/rotues/orderRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { newOrder } from "../controllers/orderControllers.js";
+import { getOrderDetails, newOrder, userOrderDetails } from "../controllers/orderControllers.js";
 import { isAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 router.route("/order/new").post(isAuthenticated ,newOrder);
+router.route("/order/:id").get(isAuthenticated , getOrderDetails);
+router.route("/me/orders").get(isAuthenticated , userOrderDetails);
 
 export default router;
