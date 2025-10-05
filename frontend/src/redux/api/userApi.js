007b121f7e8d3a8b55c7e482acb959541f91bad5
Usman3432 <u.ghani3132@gmail.com>
2025-10-05 22:03:55 +0500

    Added api for checking logged-in user

---
+++ frontend/src/redux/api/userApi.js
@@ -0,0 +1,25 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { setIsAuthenticated, setUser } from "../features/userSlice";
+
+export const userApi = createApi({
+  reducerPath: "userApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "/api/v1" }),
+  endpoints: (builder) => ({
+    getMe: builder.query({
+      query: () => `/me`,
+      transformResponse: (result) => result.user,
+      async onQueryStarted(args, { dispatch, queryFulfilled }) {
+        try {
+          const { data } = await queryFulfilled;
+          dispatch(setUser(data));
+          dispatch(setIsAuthenticated(true));
+        } catch (error) {
+          console.log(error);
+        }
+      },
+    }),
+  }),
+});
+
+//Its a hook that can be used in components to load all variables(isloading var, success var, error var etc)
+export const { useGetMeQuery } = userApi;
---
+++ frontend/src/redux/features/userSlice.js
@@ -0,0 +1,24 @@
+//A slice is a collection of our reducer logic and then the actions
+
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+};
+
+export const userSlice = createSlice({
+  initialState,
+  name: "userSlice",
+  reducers: {
+    setUser(state, action) {
+      state.user = action.payload;
+    },
+    setIsAuthenticated(state, action) {
+      state.isAuthenticated = action.payload;
+    },
+  },
+});
+
+export default userSlice.reducer;
+export const { setIsAuthenticated, setUser } = userSlice.actions;
