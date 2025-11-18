7f053eb9d08bc8952cf1cda5b7b650f50ac87a43
Usman3432 <152250676+Usman3432@users.noreply.github.com>
2025-11-18 14:10:54 +0500

    Resolved

--- backend/seeder/seeder.js
+++ backend/seeder/seeder.js
@@ -4,11 +4,14 @@ import products from "./data.js"
 
 const seedProduct = async () => {
     try {
-        await mongoose.connect("mongodb+srv://ushop_ad12:Ushop@ushop.phkaliq.mongodb.net/Ushop")
+        let DB_URI = "";
+        if(process.env.MODE_ENV === "DEVELOPMENT") DB_URI = process.env.DB_LOCAL_URI;
+        if(process.env.MODE_ENV === "PRODUCTION") DB_URI = process.env.DB_URI;
+        await mongoose.connect(DB_URI);
         await Product.deleteMany();
         console.log("Products deleted");
         await Product.insertMany(products);
-        console.log("Products added");
+        console.log("Products added successfully");
     } catch (error) {
         console.log(error.message);
     }
